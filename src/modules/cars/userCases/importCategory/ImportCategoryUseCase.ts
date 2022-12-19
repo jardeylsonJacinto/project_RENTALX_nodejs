b1941467b05f19208e4a41786a6a95feb49ebe94
Jardeylson jacinto <jardeylsong.m@gmail.com>
2022-12-19 09:55:14 -0300

    feat: add the csv-parser library

--- src/modules/cars/userCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/userCases/importCategory/ImportCategoryUseCase.ts
@@ -1,8 +1,19 @@
-class ImportCategoryUseCase {
+import csvParse from "csv-parser";
+import fs from "fs";
 
+class ImportCategoryUseCase {
   execute(file: Express.Multer.File):void {
-    console.log(file);
+
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+    
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
-export { ImportCategoryUseCase }
+export { ImportCategoryUseCase };
